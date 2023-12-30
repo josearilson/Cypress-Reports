a813f54e2e49020cf9a4c05b3affa1423dbb59a3
José Arilson <40409188+josearilson@users.noreply.github.com>
2023-12-29 23:26:52 -0300

    Atualizar o cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   video: false,
-reporter: '../node_modules/cypress-mochawesome-reporter,
+reporter: "cypress-multi-reporters",
   reporterOptions: {
     configFile: 'reporter-config.json',
   },
